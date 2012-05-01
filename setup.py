9f45d2792d746bd1eb0cd32cc3cf423ef0e38347
Clément <clement.nodet@gmail.com>
2012-05-01 18:23:05 +0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-debug-toolbar-redis',
-    version='0.0.1',
+    version='0.0.2',
     description='Simple debug toolbar panel for Redis',
     #long_description=open('README.md').read(),
     author='Clement Nodet',
