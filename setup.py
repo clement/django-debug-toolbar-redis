614d18b56d942d290d811901dd1a51a89115e70d
Clément <clement.nodet@gmail.com>
2012-05-01 18:13:49 +0700

    add a setup file

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='django-debug-toolbar-redis',
+    version='0.0.1',
+    description='Simple debug toolbar panel for Redis',
+    #long_description=open('README.md').read(),
+    author='Clement Nodet',
+    author_email='clement.nodet@gmail.com',
+    url='http://github.com/clement/django-debug-toolbar-redis',
+    #license='MIT',
+    py_modules=['redis_panel'],
+    zip_safe=True,
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Web Environment',
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+)
+
